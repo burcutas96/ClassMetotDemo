d738b2b9fc849344959d342b13383b993287a5a6
burcutas636@gmail.com <burcutas636@gmail.com>
2022-02-25 22:03:18 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri eklendi.");
+            Console.WriteLine("");
+        }
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            Console.WriteLine("Müşteri Listesi:");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad);
+            }
+            Console.WriteLine("");
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri silindi.");
+            Console.WriteLine("");
+        }
+        public void MusteriGuncelle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " adlı müşteri güncellendi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using ClassMetotDemo;
+
+MusteriManager musteriManager = new MusteriManager();
+
+Musteri musteri1 = new Musteri();
+musteri1.Id = 1;
+musteri1.Ad = "Burcu";
+musteri1.Soyad = "Taş";
+musteri1.Cinsiyet = "Kız";
+
+Musteri musteri2 = new Musteri();
+musteri2.Id = 2;
+musteri2.Ad = "Engin";
+musteri2.Soyad = "Demiroğ";
+musteri2.Cinsiyet = "Erkek";
+
+Musteri musteri3 = new Musteri();
+musteri3.Id = 3;
+musteri3.Ad = "Enes";
+musteri3.Soyad = "Bayram";
+musteri3.Cinsiyet = "Erkek";
+
+Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+musteriManager.MusteriEkle(musteri1);
+musteriManager.MusteriListele(musteriler);
+musteriManager.MusteriSilme(musteri3);
+musteriManager.MusteriGuncelle(musteri2);
+
+Console.ReadLine();
+
